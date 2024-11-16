5218f3ef198f13c4bd3902fd5d3d7fcf51471077
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-11-16 22:16:42 +0900

    型修正

--- onepeace/src/consts/index.tsx
+++ onepeace/src/consts/index.tsx
@@ -2,10 +2,8 @@ import type { naviLinkType } from '@/type/types.ts';
 
 const title: string = 'OnePeace';
 const linkEl: naviLinkType = {
-  title: { name: title, routerName: 'TOP' },
-  top: { name: 'TOP', routerName: 'TOP' },
-  about: { name: 'ABOUT', routerName: 'ABOUT' },
-  info: { name: 'INFOMATION', routerName: 'INFOMATION' },
+  title: { name: title, url: './' },
+  top: { name: 'TOP', url: './' },
 };
 
 export { linkEl };
