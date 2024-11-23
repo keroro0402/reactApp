8baa8e0a226e918dfc949ca6348bdbf66ebdbfcb
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-11-23 21:54:39 +0900

    メインコンポーネント登録

--- onepeace/src/App.tsx
+++ onepeace/src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Header.tsx';
 import Navi from './components/Navi.tsx';
+import MainView from './components/MainView.tsx';
 import Footer from './components/Footer.tsx';
 function App() {
   return (
     <>
       <Header />
       <Navi />
-      <br></br>
+      <MainView />
+
       <Footer />
     </>
   );
