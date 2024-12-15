e4b900a31f6afc8814d3e6591aa071aa474a4d8d
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-12-15 17:15:33 +0900

    Todo用コンポーネント追加

--- onepeace/src/App.tsx
+++ onepeace/src/App.tsx
@@ -1,13 +1,16 @@
-import Header from './components/Header.tsx';
-import Navi from './components/Navi.tsx';
-import MainView from './components/MainView.tsx';
-import CommentArea from './components/CommentArea.tsx';
-import Footer from './components/Footer.tsx';
+import Header from '@/components/Header';
+import Navi from '@/components/Navi';
+import MainView from '@/components/MainView';
+import CommentArea from '@/components/CommentArea';
+import TodoMain from '@/components/TodoMain';
+
+import Footer from '@/components/Footer';
 function App() {
   return (
     <>
       <Header />
       <Navi />
+      <TodoMain />
       <MainView />
       <CommentArea />
       <Footer />
